84f742f93611764a3109484b38b8f951457ecdae
lamdong <lamdong520@163.com>
2019-10-15 15:12:30 +0800

    Jenkins自动化部署

---
+++ src/main/java/com/example/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: spring-boot-jenkins
+ * @ClassName: IndexController
+ * @company: 鸿业（深圳）信息技术服务有限公司
+ * @author: Lamdong
+ * @create: 2019-10-15 15:01
+ **/
+@RestController
+public class IndexController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/index")
+    public String index(){
+        return "hello docker and jenkins......"+ serverPort;
+    }
+}
